c089e812a6f6056658e59541f01cff63ac3773b8
brebic4 <brebic@student.unipu.hr>
2025-03-25 23:05:06 +0100

    add files

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup></script>
+
+<template>
+  <div
+    class="flex flex-col justify-center items-center gap-3 bg-gradient-to-br from-emerald-950 to-indigo-950 h-full"
+  >
+    <p class="text-6xl text-white">
+      Pozdrav, ja sam
+      <span class="text-cyan-400">Bruno Rebić</span>
+    </p>
+
+    <div class="w-5xl border-2 opacity-75 rounded-full bg-white p-0.5"></div>
+
+    <p class="text-2xl text-teal-100/75">Moji omiljeni filmovi</p>
+
+    <div
+      class="flex gap-2 justify-center items-center text-6xl text-zinc-700 text-center"
+    >
+      <div>
+        <div
+          class="w-80 h-120 rounded-md bg-zinc-300 flex justify-center items-center"
+        >
+          Poster
+        </div>
+        <div class="text-white text-2xl">Naslov filma</div>
+      </div>
+
+      <div>
+        <div
+          class="w-80 h-120 rounded-md bg-zinc-300 flex justify-center items-center"
+        >
+          Poster
+        </div>
+        <div class="text-white text-2xl">Naslov filma</div>
+      </div>
+
+      <div>
+        <div
+          class="w-80 h-120 rounded-md bg-zinc-300 flex justify-center items-center"
+        >
+          Poster
+        </div>
+        <div class="text-white text-2xl">Naslov filma</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
