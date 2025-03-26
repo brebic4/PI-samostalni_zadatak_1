4ddeab54a577371d59a089664b2a2f39bd0a4d3e
brebic4 <brebic@student.unipu.hr>
2025-03-26 20:42:16 +0100

    add responsiveness

--- src/App.vue
+++ src/App.vue
@@ -2,45 +2,47 @@
 
 <template>
   <div
-    class="flex flex-col justify-center items-center gap-3 bg-gradient-to-br from-emerald-950 to-indigo-950 h-full"
+    class="flex flex-col justify-center items-center gap-3 bg-gradient-to-br from-emerald-950 to-indigo-950 h-full p-4"
   >
-    <p class="text-6xl text-white">
+    <p class="text-xl xl:text-6xl sm:text-4xl text-white">
       Pozdrav, ja sam
       <span class="text-cyan-400">Bruno Rebić</span>
     </p>
 
-    <div class="w-5xl border-2 opacity-75 rounded-full bg-white p-0.5"></div>
+    <div
+      class="w-64 xl:w-5xl md:w-2xl sm:w-xl border-2 opacity-75 rounded-full bg-white p-0.5"
+    ></div>
 
-    <p class="text-2xl text-teal-100/75">Moji omiljeni filmovi</p>
+    <p class="text-lg sm:text-2xl text-teal-100/75">Moji omiljeni filmovi</p>
 
     <div
-      class="flex gap-2 justify-center items-center text-6xl text-zinc-700 text-center"
+      class="flex gap-2 justify-center items-center text-xl xl:text-6xl sm:text-4xl text-zinc-700 text-center"
     >
       <div>
         <div
-          class="w-80 h-120 rounded-md bg-zinc-300 flex justify-center items-center"
+          class="w-20 h-30 xl:w-80 xl:h-120 sm:w-50 sm:h-70 rounded-md bg-zinc-300 flex justify-center items-center"
         >
           Poster
         </div>
-        <div class="text-white text-2xl">Naslov filma</div>
+        <div class="text-white text-xs sm:text-2xl">Naslov filma</div>
       </div>
 
       <div>
         <div
-          class="w-80 h-120 rounded-md bg-zinc-300 flex justify-center items-center"
+          class="w-20 h-30 xl:w-80 xl:h-120 sm:w-50 sm:h-70 rounded-md bg-zinc-300 flex justify-center items-center"
         >
           Poster
         </div>
-        <div class="text-white text-2xl">Naslov filma</div>
+        <div class="text-white text-xs sm:text-2xl">Naslov filma</div>
       </div>
 
       <div>
         <div
-          class="w-80 h-120 rounded-md bg-zinc-300 flex justify-center items-center"
+          class="w-20 h-30 xl:w-80 xl:h-120 sm:w-50 sm:h-70 rounded-md bg-zinc-300 flex justify-center items-center"
         >
           Poster
         </div>
-        <div class="text-white text-2xl">Naslov filma</div>
+        <div class="text-white text-xs sm:text-2xl">Naslov filma</div>
       </div>
     </div>
   </div>
